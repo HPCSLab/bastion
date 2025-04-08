6540988aaf7ef462d065addb3801137c804e3749
Masaki.Nakano <admin@namachan10777.dev>
2025-04-08 18:10:34 +0900

    create ssh key dir

--- ssh_host_keys_gen.sh
+++ ssh_host_keys_gen.sh
@@ -2,14 +2,16 @@
 
 set -eux
 
-if [ ! -e /etc/ssh/ssh_host_ecdsa_key ]; then
-  ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+mkdir -p /etc/ssh/keys
+
+if [ ! -e /etc/ssh/keys/ecdsa ]; then
+  ssh-keygen -t ecdsa -f /etc/ssh/keys/ecdsa -N ""
 fi
 
-if [ ! -e /etc/ssh/ssh_host_ed25519_key ]; then
-  ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
+if [ ! -e /etc/ssh/keys/ed25519 ]; then
+  ssh-keygen -t ed25519 -f /etc/ssh/keys/ed25519 -N ""
 fi
 
-if [ ! -e /etc/ssh/ssh_host_rsa_key ]; then
-  ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
+if [ ! -e /etc/ssh/keys/rsa ]; then
+  ssh-keygen -t rsa -f /etc/ssh/keys/rsa -N ""
 fi
